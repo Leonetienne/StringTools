5f791b8d9f66b53e1019abfe6a13b6e86fe5ca51
Leonetienne <leonetienne@hotmail.de>
2022-02-28 00:08:59 +0100

    License and doxygen setup

--- Doxygen/doxyrun.sh
+++ Doxygen/doxyrun.sh
@@ -1,1 +1,9 @@
-sudo doxygen doxyfig
+# Copy repository readme here to be used as a cover page
+tail ../readme.md -n +2 > index.md
+
+# Run doxygen
+doxygen doxyfig
+
+# Cleanup index.md
+rm -f index.md
+
